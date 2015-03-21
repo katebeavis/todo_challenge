14045f8f05e4dd0d17e174c638dd54753f3ebdde
Kate <katebeavis84@gmail.com>
2015-03-21 16:56:12 +0000

    Refactored index

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -1,12 +1,5 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name mytodoApp.controller:AboutCtrl
- * @description
- * # AboutCtrl
- * Controller of the mytodoApp
- */
 angular.module('mytodoApp')
   .controller('AboutCtrl', function ($scope) {
     $scope.awesomeThings = [
